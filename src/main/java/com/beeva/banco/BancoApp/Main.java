746c9213e572206942f463780d5603b3289414f7
EdSanchezEsc <eddiiee6@gmail.com>
2017-07-05 10:10:33 -0500

    Inicializacion de repositorio de banco
    	new file:   src/main/java/com/beeva/banco/BancoApp/App.java
    	new file:   src/main/java/com/beeva/banco/BancoApp/Main.java
    	new file:   src/test/java/com/beeva/banco/BancoApp/AppTest.java

---
+++ src/main/java/com/beeva/banco/BancoApp/Main.java
@@ -0,0 +1,13 @@
+package com.beeva.banco.BancoApp;
+
+import com.beeva.calculadora.Calculadora;
+import com.sun.org.omg.SendingContext.*;
+
+public class Main {
+
+	public static void main(String[] args) {
+		Calculadora calc = new Calculadora();
+		calc.Division(2, 2);		
+	}
+
+}
